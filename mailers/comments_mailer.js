9318272e2ff987dd3ff441ebf426556e13e2a89d
GRACY  JAYANT <40520229+gracyj@users.noreply.github.com>
2019-08-02 22:34:20 +0530

    Add files via upload

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,24 @@
+// sending our email via SMTP
+const nodeMailer = require("../config/nodemailer");
+
+
+// this is another way of exporting a method
+exports.newComment = (comment) => {
+    // console.log("inside newComment mailer", comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment},"/comments/new_comment.ejs");
+
+    nodeMailer.transporter.sendMail({
+        from: "bobbysing29@gmail.com",
+        to: comment.user.email,
+        subject: "New Comment Published",
+        // html: "<h1> yup, your comment is now published! </h1>"
+        html: htmlString
+    }, (err,info) => {
+        if(err){
+        console.log("Error in sending mail",err);
+        return;
+         }
+    // console.log("message sent", info);
+    return;
+ }); 
+}
